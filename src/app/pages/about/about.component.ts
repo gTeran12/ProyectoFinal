901b9222e6493259bc9fdca775890fad49be762e
gTeran12 <gteran@espol.edu.ec>
2023-08-30 18:53:49 -0400

    Guia19 Funcionando

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+  description: string = 'Descripción ... '; /* Cambie el texto con la descripción de su proyecto */
+
+}
